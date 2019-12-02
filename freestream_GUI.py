141b31963d2234ae0d79bee159b1d15665f8e507
DIPAK KUMAR SISODIYA <dipak.work.14@gmail.com>
2019-12-02 10:35:56 +0530

    The file will be the main go to file for matplotlib and Tkinter

---
+++ freestream_GUI.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 10:04:29 2019
+
+@author: DIPAK
+"""
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.figure import Figure
+import math
+
+
+f = Figure(figsize = (5,5), dpi =100)
+
+
