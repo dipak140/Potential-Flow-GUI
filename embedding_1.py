444d27b2d56d11b47757d655016fee1f6f33b473
DIPAK KUMAR SISODIYA <dipak.work.14@gmail.com>
2019-12-10 22:14:01 +0530

    Commit on 10 December 2019

--- embedding_1.py
+++ embedding_1.py
@@ -13,19 +13,12 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationTool
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import numpy as np
-import sourceSink
+import sourceSink.py
 
 
 root = tkinter.Tk()
 root.wm_title("Embedding in Tk")
 
-u_source, v_source = sourceSink.Source()
-x_start, x_end = -2.0, 2.0 
-y_start, y_end = -1.0 , 1.0
-height= (y_end- y_start) /(x_end - x_start) * 10.0
-X, Y = sourceSink.pos()
-f = plt.streamplot(X,Y, u_source, v_source,  density = 2,  linewidth = 1,  arrowsize = 2,  arrowstyle = '->')
-canvas = FigureCanvasTkAgg(f, master=root)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+u_source, v_source = sourceSink.source()
+
 tkinter.mainloop()
